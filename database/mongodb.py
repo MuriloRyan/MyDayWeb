0a7cacca877a8abc4bbbca19f46c8a28eed55a99
Murilo Ryan <murilryanbarbosa@gmail.com>
2023-06-23 11:45:24 -0300

    First deploy

---
+++ database/mongodb.py
@@ -0,0 +1,94 @@
+from hashlib import sha3_512
+try:
+    from database.attempts import addAttemp
+    from database.userconfig import User
+except:
+    from attempts import addAttemp
+    from userconfig import User
+
+import pymongo
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# URL TO CONNECT
+url = os.getenv('MONGO_URL')
+
+# Connect to the database
+Cluster = pymongo.MongoClient(url)
+database = Cluster.get_database('MydayDb')
+db = database.get_collection('users')
+
+def signIn(data):
+
+    userTest = db.find_one({
+        'username': data['username'],
+        'email': data['email'],
+        'password': data['password']
+        })
+    
+    if userTest:
+        return None #incorrect request
+    else:
+        db.insert_one(data)
+        return 200 #ok request
+
+def logIn(data):
+    userAtempt = db.find_one({'email': data['email'], 'password': data['password']})
+
+    if userAtempt:
+        return addAttemp(data)
+    
+    return None
+
+
+def findIn(data,user):
+    userTest = db.find_one({'email': user})
+
+    if userTest:
+        testReturn = {}
+        for key in data:
+            testReturn[key] = userTest.get(key)
+        return testReturn
+    else:
+        return None
+
+def addTask(user, taskdata):
+    # taskdata = {'taskname', 'comment'}
+
+    userTest = db.find_one({'email': user['email']})
+
+    if userTest:
+        db.update_one({'_id': userTest['_id']}, {'$push': {'tasks': taskdata}})
+        return 200
+    else:
+        return None
+
+def getTask(user):
+
+    userTest = db.find_one({'email': user['email']})
+
+    if userTest:
+        return userTest['tasks']
+    else:
+        return None
+
+def killTask(user, taskname):
+    userTest = db.find_one({'email': user['email']})
+
+    if userTest:
+        tasks = userTest['tasks']
+        taskIndex = None
+        for index, task in enumerate(tasks):
+            if task['taskname'] == taskname:
+                taskIndex = index
+                break
+
+        if taskIndex is not None:
+            tasks.pop(taskIndex)  # Remove a tarefa da lista
+            db.update_one({'_id': userTest['_id']}, {'$set': {'tasks': tasks}})
+            
+            return True
+
+    return None
---
+++ main.py
@@ -0,0 +1,131 @@
+from flask import Flask, render_template,request, redirect, session
+from flask_session import Session
+from database.userconfig import User
+import database.mongodb as db
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SESSION_KEY') 
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+""" ___________________ """
+"""                     """
+""" FRONTEND OPERATIONS """
+""" ___________________ """
+@app.route('/')
+def homepage():
+    if 'email' in session:
+        email = session['email']
+        taskdata = db.getTask({'email': email})
+        return render_template('home.html', taskdata=taskdata)
+    else:
+        return render_template('home.html', notConnected=True)
+
+@app.route('/profile')
+def profile():
+
+    return render_template('profile.html')
+
+@app.route('/user/mytasks/', methods=['GET', 'POST'])
+def taskPage():
+
+    taskname = request.args.get('taskname')
+
+    AllTasks = db.getTask({'email': session['email']})
+    for task in AllTasks:
+        if task['taskname'] == taskname:
+            TaskData = task
+
+    return render_template('taskpage.html',taskdata=TaskData)
+
+@app.route('/newtask')
+def newtask():
+
+    return render_template('newtask.html')
+
+@app.route('/auth/login/new')
+def loginPage():
+
+    return render_template('login.html')
+
+@app.route('/auth/register/new')
+def signUpPage():
+
+    return render_template('signup.html')
+
+@app.route('/auth/checksession')
+def checkSession():
+    try:
+        session['email']
+
+        return redirect('/profile')
+    except:
+        return redirect('/auth/login/new')
+
+""" __________________ """
+"""                    """
+""" BACKEND OPERATIONS """
+""" __________________ """
+@app.route('/auth/register/api/mongodb',methods=["POST"])
+def databaseSignUp():
+    
+    data = {
+            'username': request.form['username'],
+            'email': request.form['email'],
+            'password': request.form['password']
+           }
+
+    toSignUp = User(data['username'],data['email'],data['password'])
+    db.signIn(toSignUp.get_data())
+
+    return redirect('/auth/login/new')
+
+@app.route('/auth/login/api/mongodb', methods=["POST"])
+def databaseLogin():
+    data = {
+        'email': request.form['email'],
+        'password': request.form['password']
+    }
+
+    toLogIN = User('__null__', data['email'], data['password'])
+    tryData = db.logIn(toLogIN.get_data())
+
+    if tryData is not None:
+        session['username'] = db.findIn({'username'}, data['email'])['username']
+        session['email'] = data['email']
+        session['sessionhash'] = tryData
+        return redirect('/')
+    else:
+        return redirect('/auth/login/new')
+
+@app.route('/function/session/exit')
+def sessionExit():
+    session.clear()
+    
+    return redirect('/')
+
+@app.route('/function/api/mongodb/newtask', methods=["POST"])
+def databseNewTask():
+
+    taskname = request.form['newTaskName']
+    taskcomment = request.form['newTaskComment']
+
+    db.addTask({'email': session['email']},{'taskname': taskname,'comment': taskcomment})
+
+    return redirect('/')
+
+@app.route('/function/api/mongodb/killtask/', methods=["POST"])
+def databaseKillTask():
+
+    taskToKill = request.args.get('taskname')
+
+    db.killTask({'email': session['email']},taskToKill)
+
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
